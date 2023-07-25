ab7bda17b6417452d0b47b56641e778929d3e8e5
nalaka <nalaka@intervest.lk>
2023-07-25 16:12:54 +0530

    main:added question button story

--- src/components/button/Button.stories.ts
+++ src/components/button/Button.stories.ts
@@ -1,38 +1,54 @@
-import Button from "./button"
-import type { Meta, StoryObj } from '@storybook/react';
+import Button from "./button";
+import type { Meta, StoryObj } from "@storybook/react";
 
-const meta:Meta<typeof Button>={
+const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
-  argTypes: { 
-  btnStyles:{control:"object"},
-  labelStyles:{control:"object"},
-  subLabelStyles:{control:"object"},
-  label: { control: 'text' },
-  subLabel: { control: 'text' },
-  isActive: { control: 'boolean' },
-  background:{control:'color'},
-  activeBackground:{control:'color'},
-},
-  
-}
-export default meta
+  argTypes: {
+    btnStyles: { control: "object" },
+    labelStyles: { control: "object" },
+    subLabelStyles: { control: "object" },
+    label: { control: "text" },
+    subLabel: { control: "text" },
+    isActive: { control: "boolean" },
+    background: { control: "color" },
+    activeBackground: { control: "color" },
+  },
+};
+export default meta;
 
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>;
 
-export const Default:Story={
-args :{
-  label:"test"
-}}
+export const Default: Story = {
+  args: {
+    label: "test",
+  },
+};
 
-export const ButtonWithSubLabel :Story= {
-  args :{
-    label:"test",
-    subLabel:"sub label"
-  }
-}
+export const ButtonWithSubLabel: Story = {
+  args: {
+    label: "test",
+    subLabel: "sub label",
+  },
+};
 
-
+export const QuestionButton: Story = {
+  args: {
+    label: "Single Trip",
+    subLabel: "sub label",
+    btnStyles: {
+      color: "#1173d5",
+      fontSize: "20px",
+      fontWeight: 600,
+      padding: "18px",
+      width: "100%",
+      border: "2px solid #dbdddd",
+      outLine: "none",
+      borderRadius: "8px",
+      lineHeight: "20px",
+    },
+  },
+};
 
 /* export const ButtonColourRed :Story= Template.bind({})
 ButtonColourRed.args = {
@@ -43,5 +59,3 @@ export const CellBackground = Template.bind({})
 CellBackground.args = {
   cellBackground:"#1E90FF"
 } */
-
-
