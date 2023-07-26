488f35ff6b0dd80f43a03200232485cde9abb4f6
nalaka <nalaka@intervest.lk>
2023-07-26 08:59:33 +0530

    main:added styles for button story

--- src/components/button/Button.stories.ts
+++ src/components/button/Button.stories.ts
@@ -4,39 +4,71 @@ import type { Meta, StoryObj } from "@storybook/react";
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
-  argTypes: { 
-  btnStyles:{control:"object"},
-  labelStyles:{control:"object"},
-  subLabelStyles:{control:"object"},
-  label: { control: 'text' },
-  subLabel: { control: 'text' },
-  isActive: { control: 'boolean' },
-  background:{control:'color'},
-  activeBackground:{control:'color'},
-  border: { control: 'text' },
-  activeBorder: { control: 'text' },
-  hoverStyles:{control:"object"},
-  activeStyles:{control:"object"},
-},
-  
-}
-export default meta
+  argTypes: {
+    btnStyles: { control: "object" },
+    labelStyles: { control: "object" },
+    subLabelStyles: { control: "object" },
+    label: { control: "text" },
+    subLabel: { control: "text" },
+    isActive: { control: "boolean" },
+    background: { control: "color" },
+    activeBackground: { control: "color" },
+    border: { control: "text" },
+    activeBorder: { control: "text" },
+    hoverStyles: { control: "object" },
+    activeStyles: { control: "object" },
+  },
+};
+export default meta;
 
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>;
 
-export const Default:Story={
-args :{
-  label:"test"
-}}
+export const Default: Story = {
+  args: {
+    label: "test",
+  },
+};
 
-export const ButtonWithSubLabel :Story= {
-  args :{
-    label:"test",
-    subLabel:"sub label"
-  }
-}
+export const ButtonWithSubLabel: Story = {
+  args: {
+    label: "test",
+    subLabel: "sub label",
+  },
+};
 
-
+export const QuestionButton: Story = {
+  args: {
+    label: "Single Trip",
+    labelStyles: { margin: "0", marginBottom: "10px" },
+    subLabel: "sub label",
+    subLabelStyles: {
+      margin: "0",
+      fontWeight: 400,
+      fontSize: "15px",
+      marginBottom: "0px",
+      color: "#4d5154",
+    },
+    btnStyles: {
+      color: "#1173d5",
+      fontSize: "20px",
+      fontWeight: 600,
+      padding: "18px",
+      maxWidth:"500px",
+      width: "100%",
+      border: "2px solid #dbdddd",
+      outLine: "none",
+      borderRadius: "8px",
+      lineHeight: "20px",
+    },
+    border: "2px solid #dbdddd",
+    hoverStyles: {
+      border: "2px solid #1173d5",
+    },
+    //background:"#1173d5",
+    activeBackground:"red",
+    activeBorder:"red",
+  },
+};
 
 /* export const ButtonColourRed :Story= Template.bind({})
 ButtonColourRed.args = {
